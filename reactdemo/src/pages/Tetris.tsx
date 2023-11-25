ea5635938615ada55fea7e0bdcc43cee59772b5d
cheng87126 <cheng87126@gmail.com>
2023-11-25 22:36:42 +0800

    update

--- reactdemo/src/pages/Tetris.tsx
+++ reactdemo/src/pages/Tetris.tsx
@@ -4,30 +4,80 @@ const TETROMINO = ['I','J','L','O','S','T','Z']
 export default function(){
   const currentLineRef = useRef(0)
   const [speed, setSpeed] = useState(1500)
-  const [grid, setGrid] = useState(Array(20).fill([]).map(_=>Array(10).fill(0)))
+  const [grid, setGrid] = useState<number[][]>(Array(20).fill([]).map(_=>Array(10).fill(0)))
 
-  useEffect(()=>{
-    const timer = setInterval(()=>{
-      setGrid(val=>{
-        const newVal = val.map((row,i)=>{
-          const col:(0|1|2)[] = row.map(col=>col<2?0:2)
-          if(i<=currentLineRef.current && i>=currentLineRef.current-3){
-            col[4] = 1
-          }
-          return col
-        })
+  const up = function(){
 
-        const nextLine = currentLineRef.current+1
-        currentLineRef.current = nextLine>19?0:nextLine
-
-        return newVal
+  }
+  const right = function(){
+    let newVal = grid.map((row,i)=>{
+      const idx = (row as any).findLastIndex((item:any)=>item===1)
+      if(idx!==-1 && idx<row.length-1){
+        row.unshift(0)
+        row.length = 10
+      }
+      return row
+    })
+    setGrid(newVal)
+  }
+  const down = function(){
+    const nextLine = currentLineRef.current+1
+    let flag = true
+    let newVal = grid.map((row,i)=>{
+      const col:(0|1|2)[] = row.map(col=>col<2?0:2)
+      // 
+      if(i<=currentLineRef.current && i>=currentLineRef.current-3){
+        col[4] = 1
+      }
+      // 
+      if(currentLineRef.current===19){
+        return col.map(col=>col===1?2:0)
+      }
+      return col
+    })
+    if(nextLine<19){
+      newVal[currentLineRef.current].forEach((item,idx)=>{
+        if(newVal[nextLine][idx] === 2 && item === 1){
+          flag = false
+        }
       })
-    },speed)
-
-    return ()=>{
-      clearInterval(timer)
+      if(!flag){
+        newVal = newVal.map((row,i)=>{
+          return row.map(col=>col>0?2:0)
+        })
+        setGrid(newVal)
+        currentLineRef.current = 0
+        return
+      }
     }
-  },[speed])
+    setGrid(newVal)
+    currentLineRef.current = nextLine>19?0:nextLine
+  }
+  const left = function(){
+
+  }
+  useEffect(()=>{
+    const keydown = function(e:KeyboardEvent){
+      switch(e.key){
+        case 'ArrowUp':
+          up()
+          break
+        case 'ArrowRight':
+          right()
+          break
+        case 'ArrowDown':
+          down()
+          break
+        case 'ArrowLeft':
+          left()
+          break
+      }
+    }
+    document.addEventListener('keydown',keydown,false)
+    return function(){
+      document.removeEventListener('keydown',keydown,false)
+    }
+  },[down])
 
   return (
     <section>
@@ -39,8 +89,9 @@ export default function(){
                 <div
                   key={colIdx}
                   data-col={`${rowIdx}-${colIdx}`}
-                  style={{width:'30px',height:'30px',background:col?'#000':'#fff'}}
+                  style={{width:'40px',height:'40px',background:col?'#000':'#fff'}}
                 >
+                  <div style={{color:'#666'}}>{col}</div>
                   <span style={{fontSize:'12px'}}>{`${rowIdx}-${colIdx}`}</span>
                 </div>
               )
