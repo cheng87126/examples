4809a49e59d54b65b7fb5c86411807b20f9f6a0e
cheng87126 <cheng87126@gmail.com>
2025-01-31 15:24:47 +0800

    update

--- rust/hello/simple_linked_list/src/main.rs
+++ rust/hello/simple_linked_list/src/main.rs
@@ -1,4 +1,3 @@
-use std::mem;
 #[derive(Debug)]
 pub struct ListNode<T>{
     value: T,
@@ -10,7 +9,7 @@ pub struct SimpleLinkedList<T> {
     head: Option<Box<ListNode<T>>>
 }
 
-impl<T:std::fmt::Debug> SimpleLinkedList<T> {
+impl<T> SimpleLinkedList<T> {
     pub fn new() -> Self {
         SimpleLinkedList {
             size:0,
@@ -28,125 +27,68 @@ impl<T:std::fmt::Debug> SimpleLinkedList<T> {
 
     pub fn push(&mut self, element: T) {
         self.size += 1;
-        match self.head {
-            Some(ref mut head) => {
-                let mut curr = &mut head.next;
-                while let Some(n) = curr {
-                    curr = &mut n.next;
-                }
-                *curr = Some(Box::new(
-                    ListNode {
-                        value:element,
-                        next:None
-                    }
-                ));
-            },
-            None => {
-                self.head = Some(Box::new(ListNode {
-                    value:element,
-                    next:None
-                }));
+        self.head = Some(Box::new(
+            ListNode {
+                value:element,
+                next:self.head.take()
             }
-        }
-        /*
-        if self.value.is_none() {
-            self.value = Some(element);
-        }else {
-            let mut next = &mut self.next;
-            while let Some(n) = next {
-                next = &mut n.next;
-            }
-            let node = SimpleLinkedList {
-                value:Some(element),
-                next:None
-            };
-            *next = Some(Box::new(node));
-        }
-        */
-        // println!("{:?}",self)
+        ));
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        match self.head {
-            Some(ref mut head) => {
-                self.size -= 1;
-                let head_ptr: *mut Box<ListNode<T>> = &mut *head;
-                unsafe {
-                    let h = **head_ptr;
-                }
-                /*
-                let mut prev = &mut *head;
-                let mut curr = &mut head.next;
-                while curr.is_some() && curr.unwrap().next.is_some() {
-                    let prev = *curr;
-                    curr = &mut curr.unwrap().next;
-                }
-                */
-                None
-            },
-            None => None
-        }   
-        /*
-        match mem::replace(&mut self.head, None) {
-            None => None,
-            Some(node) => {
-                self.head = node.next;
-                Some(node.value)
-            }
-        }
-        */
-        /*
-        if self.value.is_none() {
+        if self.is_empty() {
             None
         }else {
-            let prev = &mut *self;
-            let mut next = &self.next;
-            while let Some(n) = next {
-                println!("{:?}---n---{:?}",prev,n);
-                *prev = **n;
-                next = &mut n.next;
-            }
-            *next = None;
-            None
+            self.size -= 1;
+            let head = self.head.take().unwrap();
+            self.head = head.next;
+            Some(head.value)
         }
-        */
     }
 
     pub fn peek(&self) -> Option<&T> {
-        None
-        /*
-        if self.value.is_none() {
-            None
-        }else {
-            let mut next = &self.next;
-            while let Some(n) = next {
-                next = &n.next;
-                // if next.is_none() {
-                //     return Some(&n.value);
-                // }
+        match self.head {
+            None => None,
+            Some(ref head) => {
+                let node = head.as_ref();
+                Some(&node.value)
             }
-            return None;
         }
-        */
     }
 
     #[must_use]
     pub fn rev(self) -> SimpleLinkedList<T> {
-        todo!()
+        let mut link = SimpleLinkedList::new();
+        let mut curr = self.head;
+        while let Some(n) = curr {
+            link.push(n.value);
+            curr = n.next;
+        }
+        link
     }
 }
 
 impl<T> FromIterator<T> for SimpleLinkedList<T> {
-    fn from_iter<I: IntoIterator<Item = T>>(_iter: I) -> Self {
-        todo!()
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        iter
+            .into_iter()
+            .fold(SimpleLinkedList::new(), |mut acc, x| {
+                acc.push(x);
+                acc
+            })
     }
 }
 impl<T> From<SimpleLinkedList<T>> for Vec<T> {
-    fn from(mut _linked_list: SimpleLinkedList<T>) -> Vec<T> {
-        todo!()
+    fn from(mut linked_list: SimpleLinkedList<T>) -> Vec<T> {
+        let mut ret = vec![];
+        let mut curr = linked_list.head;
+        while let Some(n) = curr {
+            ret.push(n.value);
+            curr = n.next;
+        }
+        ret
     }
 }
-
 
 fn main() {
     let mut nodes = SimpleLinkedList::new();
@@ -154,5 +96,6 @@ fn main() {
     nodes.push(1);
     nodes.push(2);
     println!("{:?}",nodes);
-    
+    let rev = nodes.rev();
+    println!("{:?}",rev);
 }
